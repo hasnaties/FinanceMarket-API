171887f4b9b6adff5b523cf6000cf28369ed62da
hasnaties <hasnaties@gmail.com>
2024-03-14 10:33:38 +0500

    Middleware:Auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,19 @@
+import Jwt from "jsonwebtoken";
+import Profile from "../models/profileModel.js";
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace("Bearer ", "");
+    const decoded = Jwt.verify(token, "financemarket");
+    const profile = await Profile.findOne({ '_id': decoded._id, 'tokens.token': token });
+
+    req.token = token;
+    req.profile = profile;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Authorization failed." })
+  }
+};
+
+export default auth;
